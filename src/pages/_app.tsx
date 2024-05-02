ecea1e3a9d8a18caf3f09fac291d893d9f0bb67d
Aman Dangol <icrextha@gmail.com>
2024-05-03 02:13:54 +0545

    deployed

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,7 +5,7 @@ import { GoogleOAuthProvider } from "@react-oauth/google";
 import Head from "next/head";
 
 export default function App({ Component, pageProps }: AppProps) {
-  const clientId = process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID;
+  const clientId ="701015667868-v0hnacmbhhn135k71bpct5ejque3u1af.apps.googleusercontent.com";
 
   return (
     <>
